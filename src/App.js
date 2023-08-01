5417490714f236598c4546d4425d0ca2473acc54
Giedre <giedre.rageliene@gmail.com>
2023-08-01 13:32:27 +0300

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+import style from './App.module.css';
+import { Home } from "./pages/Home";
+import { Page404 } from "./pages/Page404";
+import { About } from "./pages/About";
+import { Services } from "./pages/Services";
+import { Service } from "./pages/Service";
+import { Layout } from "./layout/Layout";
+import { Layout2 } from "./layout/Layout2";
 
 function App() {
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const fullname = `${name} ${surname}`.trim();
+
+  function updateName(event) {
+    setName(event.target.value);
+  }
+
+  function updateSurname(event) {
+    setSurname(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.app}>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={Layout}>
+            <Route index path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/services/:serviceId" element={<Service />} />
+            <Route path="/services/:serviceId/edit" element={<Service />} />
+            <Route path="/services/:serviceId/providers/:name" element={<Service />} />
+          </Route>
+          <Route Component={Layout2}>
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Page404 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+
+      {/* <form className={style.form}>
+        <div className={style.row}>
+          <label htmlFor='name'>Name</label>
+          <input onChange={updateName} value={name} id='name' type='text' placeholder='Name...' />
+        </div>
+        <div className={style.row}>
+          <label htmlFor='surname'>Surname</label>
+          <input onChange={updateSurname} value={surname} id='surname' type='text' placeholder='Surname...' />
+        </div>
+        <div className={style.row}>
+          <button type='submit'>Create</button>
+        </div>
+      </form>
+
+      <div className={style.preview}>"{fullname}"</div> */}
     </div>
   );
 }
